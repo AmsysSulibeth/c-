b2982de56e5913810546de112d3fb72a231c3ce7
AmsysSulibeth <167362126+AmsysSulibeth@users.noreply.github.com>
2024-04-19 06:35:55 +1030

    expresionm4

---
+++ expresionm4/expresionm4/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace expresionm4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtA = new System.Windows.Forms.TextBox();
+            this.TxtB = new System.Windows.Forms.TextBox();
+            this.TxtC = new System.Windows.Forms.TextBox();
+            this.TxtD = new System.Windows.Forms.TextBox();
+            this.btlimpiar = new System.Windows.Forms.Button();
+            this.btcalcular = new System.Windows.Forms.Button();
+            this.btsalir = new System.Windows.Forms.Button();
+            this.A = new System.Windows.Forms.Label();
+            this.D = new System.Windows.Forms.Label();
+            this.C = new System.Windows.Forms.Label();
+            this.B = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.Txtresultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(203, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(369, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Expresiones Matematicas ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(51, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(213, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese el valor de (A):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(52, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(213, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ingrese el valor de (B):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(52, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(213, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ingrese el valor de (C):";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(52, 305);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(213, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ingrese el valor de (D):";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // TxtA
+            // 
+            this.TxtA.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtA.Location = new System.Drawing.Point(277, 117);
+            this.TxtA.Name = "TxtA";
+            this.TxtA.Size = new System.Drawing.Size(100, 32);
+            this.TxtA.TabIndex = 5;
+            // 
+            // TxtB
+            // 
+            this.TxtB.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtB.Location = new System.Drawing.Point(277, 179);
+            this.TxtB.Name = "TxtB";
+            this.TxtB.Size = new System.Drawing.Size(100, 32);
+            this.TxtB.TabIndex = 6;
+            // 
+            // TxtC
+            // 
+            this.TxtC.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtC.Location = new System.Drawing.Point(277, 241);
+            this.TxtC.Name = "TxtC";
+            this.TxtC.Size = new System.Drawing.Size(100, 32);
+            this.TxtC.TabIndex = 7;
+            // 
+            // TxtD
+            // 
+            this.TxtD.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtD.Location = new System.Drawing.Point(277, 303);
+            this.TxtD.Name = "TxtD";
+            this.TxtD.Size = new System.Drawing.Size(100, 32);
+            this.TxtD.TabIndex = 8;
+            // 
+            // btlimpiar
+            // 
+            this.btlimpiar.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btlimpiar.ForeColor = System.Drawing.Color.Red;
+            this.btlimpiar.Location = new System.Drawing.Point(350, 380);
+            this.btlimpiar.Name = "btlimpiar";
+            this.btlimpiar.Size = new System.Drawing.Size(129, 45);
+            this.btlimpiar.TabIndex = 9;
+            this.btlimpiar.Text = "Limpiar";
+            this.btlimpiar.UseVisualStyleBackColor = true;
+            this.btlimpiar.Click += new System.EventHandler(this.btlimpiar_Click);
+            // 
+            // btcalcular
+            // 
+            this.btcalcular.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btcalcular.ForeColor = System.Drawing.Color.Red;
+            this.btcalcular.Location = new System.Drawing.Point(209, 380);
+            this.btcalcular.Name = "btcalcular";
+            this.btcalcular.Size = new System.Drawing.Size(119, 45);
+            this.btcalcular.TabIndex = 10;
+            this.btcalcular.Text = "Calcular";
+            this.btcalcular.UseVisualStyleBackColor = true;
+            this.btcalcular.Click += new System.EventHandler(this.btcalcular_Click);
+            // 
+            // btsalir
+            // 
+            this.btsalir.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btsalir.ForeColor = System.Drawing.Color.Red;
+            this.btsalir.Location = new System.Drawing.Point(501, 380);
+            this.btsalir.Name = "btsalir";
+            this.btsalir.Size = new System.Drawing.Size(119, 45);
+            this.btsalir.TabIndex = 11;
+            this.btsalir.Text = "Salir";
+            this.btsalir.UseVisualStyleBackColor = true;
+            this.btsalir.Click += new System.EventHandler(this.btsalir_Click);
+            // 
+            // A
+            // 
+            this.A.AutoSize = true;
+            this.A.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A.Location = new System.Drawing.Point(461, 151);
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(29, 27);
+            this.A.TabIndex = 12;
+            this.A.Text = "A";
+            // 
+            // D
+            // 
+            this.D.AutoSize = true;
+            this.D.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.D.Location = new System.Drawing.Point(526, 220);
+            this.D.Name = "D";
+            this.D.Size = new System.Drawing.Size(29, 27);
+            this.D.TabIndex = 13;
+            this.D.Text = "D";
+            // 
+            // C
+            // 
+            this.C.AutoSize = true;
+            this.C.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C.Location = new System.Drawing.Point(461, 220);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(28, 27);
+            this.C.TabIndex = 14;
+            this.C.Text = "C";
+            // 
+            // B
+            // 
+            this.B.AutoSize = true;
+            this.B.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B.Location = new System.Drawing.Point(527, 151);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(28, 27);
+            this.B.TabIndex = 15;
+            this.B.Text = "B";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(498, 151);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(26, 27);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "+";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(498, 220);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(26, 27);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "+";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(457, 178);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(98, 23);
+            this.label12.TabIndex = 18;
+            this.label12.Text = "________";
+            this.label12.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(574, 178);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(26, 27);
+            this.label13.TabIndex = 19;
+            this.label13.Text = "=";
+            // 
+            // Txtresultado
+            // 
+            this.Txtresultado.Enabled = false;
+            this.Txtresultado.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txtresultado.Location = new System.Drawing.Point(606, 173);
+            this.Txtresultado.Name = "Txtresultado";
+            this.Txtresultado.Size = new System.Drawing.Size(182, 32);
+            this.Txtresultado.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Txtresultado);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.B);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.D);
+            this.Controls.Add(this.A);
+            this.Controls.Add(this.btsalir);
+            this.Controls.Add(this.btcalcular);
+            this.Controls.Add(this.btlimpiar);
+            this.Controls.Add(this.TxtD);
+            this.Controls.Add(this.TxtC);
+            this.Controls.Add(this.TxtB);
+            this.Controls.Add(this.TxtA);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Amsys Expresiones M";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtA;
+        private System.Windows.Forms.TextBox TxtB;
+        private System.Windows.Forms.TextBox TxtC;
+        private System.Windows.Forms.TextBox TxtD;
+        private System.Windows.Forms.Button btlimpiar;
+        private System.Windows.Forms.Button btcalcular;
+        private System.Windows.Forms.Button btsalir;
+        private System.Windows.Forms.Label A;
+        private System.Windows.Forms.Label D;
+        private System.Windows.Forms.Label C;
+        private System.Windows.Forms.Label B;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox Txtresultado;
+    }
+}
+
---
+++ expresionm4/expresionm4/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace expresionm4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btsalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btlimpiar_Click(object sender, EventArgs e)
+        {
+            TxtA.Clear();
+            TxtB.Clear();
+            TxtC.Clear();
+            TxtD.Clear();
+            Txtresultado.Clear();
+            TxtA.Focus();
+        }
+
+        private void btcalcular_Click(object sender, EventArgs e)
+        {
+            double a1, b1, c1, d1, resultado = 0;
+
+            a1 = Convert.ToDouble(TxtA.Text);
+            b1 = Convert.ToDouble(TxtB.Text);
+            c1 = Convert.ToDouble(TxtC.Text);
+            d1 = Convert.ToDouble(TxtD.Text);
+
+
+            resultado = (a1 + b1) / (c1 + d1);
+
+            Txtresultado.Text = Convert.ToString(resultado);
+        }
+    }
+}
